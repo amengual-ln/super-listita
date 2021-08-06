3e269a5d6cb2af2935de9b3b96c98e05342b21af
Lautaro Amengual <amengual.ln@gmail.com>
2021-08-06 02:30:40 -0300

    Created the store, products selectors and reducers

---
+++ store/reducers/products.js
@@ -0,0 +1,77 @@
+import { db } from '../../db/firebase'
+
+let initialState = {
+	items: [],
+	status: 'idle',
+	error: null,
+}
+
+export default function state(state = initialState, action) {
+	if (action.type === '@products/SET_PRODUCTS') {
+		action.payload.forEach((product) => {
+			if (
+				!state.items.find(
+					(previousProduct) => previousProduct.id === product.id
+				)
+			) {
+				state.items.push({
+					id: product.id,
+					...product.data(),
+				})
+			}
+		})
+	}
+	if (action.type === '@products/CREATE_PRODUCT') {
+		state.items.push(action.payload)
+	}
+	if (action.type === '@products/TOGGLE_PURCHASED') {
+		state.items = state.items.map((item) => {
+			if (item.id === action.payload) {
+				return {
+					...item,
+					comprado: !item.comprado
+				}
+			}
+			return item
+		})
+	}
+	if (action.type === '@products/SET_LOADING') {
+		state.status = action.payload
+	}
+	return state
+}
+
+/////////////////////
+
+export const createProduct = (product) => async (dispatch, getState) => {
+	try {
+		const ref = await db.collection('producto').add(product)
+		dispatch({
+			type: '@products/CREATE_PRODUCT',
+			payload: { ...product, id: ref.id },
+		})
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+export const fetchProducts = () => {
+	return async (dispatch) => {
+		const snapshot = await db.collection('producto').get()
+		dispatch({ type: '@products/SET_PRODUCTS', payload: snapshot.docs })
+	}
+}
+
+export const togglePurchased = (id, comprado) => async (dispatch, getState) => {
+	db.collection('producto').doc(id).set(
+		{
+			comprado: !comprado,
+		},
+		{ merge: true }
+	)
+	dispatch({ type: '@products/TOGGLE_PURCHASED', payload: id })
+}
+
+export const toggleLoading = (status) => async (dispatch, getState) => {
+	dispatch({ type: '@products/SET_LOADING', payload: status })
+}
---
+++ store/selectors/products.js
@@ -0,0 +1,13 @@
+export const getProductsState = (store) => store.products
+
+export const getAllProducts = (store) =>
+	getProductsState(store).items ? getProductsState(store).items : []
+
+export const getProducts = (store) =>
+	getProductsState(store).items ? getProductsState(store).items.filter(product => !product.comprado) : []
+
+export const getPurchasedProducts = (store) =>
+	getProductsState(store).items ? getProductsState(store).items.filter(product => product.comprado) : []
+
+export const getProduct = (store, id) =>
+	getProductsState(store)?.find((product) => product.id === id)
