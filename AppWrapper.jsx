52f6af103e8fdf39ba8ee55ea5f376230dd0d89f
Lautaro Amengual <amengual.ln@gmail.com>
2021-08-06 02:30:59 -0300

    App wraper for the store provider

---
+++ AppWrapper.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import App from './App'
+import { Provider } from 'react-redux'
+import store from './store'
+
+const AppWrapper = () => {
+	return <Provider store={store}><App /></Provider>
+}
+
+export default AppWrapper
