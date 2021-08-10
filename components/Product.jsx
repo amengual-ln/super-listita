e3552ddc697c07f793ebcec03383192a769a2888
Lautaro Amengual <amengual.ln@gmail.com>
2021-08-10 18:39:11 -0300

    Implemented swipe to delete

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { togglePurchased } from '../store/reducers/products'
+import { togglePurchased, removeProduct } from '../store/reducers/products'
 import {
 	StyleSheet,
 	Text,
 	Vibration,
 	View,
 	TouchableOpacity,
+	Dimensions,
 } from 'react-native'
+
+import { PanGestureHandler } from 'react-native-gesture-handler'
+import Animated, {
+	useAnimatedGestureHandler,
+	useAnimatedStyle,
+	useSharedValue,
+	withSpring,
+} from 'react-native-reanimated'
 
 import Icon from 'react-native-vector-icons/Octicons'
 const checkIcon = <Icon name="check" size={30} color="#fff" />
@@ -50,15 +59,48 @@ const Product = ({ id, text, comprado = false }) => {
 		dispatch(togglePurchased(id, comprado))
 	}
 
+	const translateX = useSharedValue(0)
+
+	const panGestureEvent = useAnimatedGestureHandler({
+		onStart: (event, context) => {
+			context.translateX = translateX.value
+		},
+		onActive: (event, context) => {
+			translateX.value = event.translationX + context.translateX
+		},
+		onEnd: (event, context) => {
+			const screenWidth = Dimensions.get('window').width
+			const distance = Math.abs(translateX.value)
+			if (distance > screenWidth / 2) {
+				translateX.value = withSpring(translateX.value * 2)
+				dispatch(removeProduct(id))
+			} else {
+				translateX.value = withSpring(0, { stiffness: 150, mass: 0.5 })
+			}
+		},
+	})
+
+	const animationStyle = useAnimatedStyle(() => {
+		return {
+			transform: [
+				{
+					translateX: translateX.value,
+				},
+			],
+		}
+	})
+
 	return (
-		<View style={styles.producto}>
-			<Text style={comprado ? styles.nombreComprado : styles.nombre}>
-				{text}
-			</Text>
-			<TouchableOpacity onPress={() => toggleEstado()}>
-				<View style={styles.check}>{checkIcon}</View>
-			</TouchableOpacity>
-		</View>
+		<PanGestureHandler onGestureEvent={panGestureEvent}>
+			<Animated.View style={[styles.producto, animationStyle]}>
+				<Text style={comprado ? styles.nombreComprado : styles.nombre}>
+					{text}
+				</Text>
+				<TouchableOpacity onPress={() => toggleEstado()}>
+					<View style={styles.check}>{checkIcon}</View>
+				</TouchableOpacity>
+			</Animated.View>
+		</PanGestureHandler>
 	)
 }
 
